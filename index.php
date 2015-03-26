c8bfdacaad99b488f3030556cd89296289394482
Kunaal <kunaalus@gmail.com>
2015-03-26 06:08:20 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!-- 
+	Made with love by Kunaal Jain
+	Email: kunaalus@gmail.com 
+-->
+
+<html>
+ 
+<head>  
+<title>File Uploader|Kunaal </title> 
+<link href="dropzone.min.css" type="text/css" rel="stylesheet" />
+<link href="basic.min.css" type="text/css" rel="stylesheet" />
+<script src="dropzone.min.js"></script>
+</head>
+ 
+<body>
+ <form action="upload.php" class="dropzone"></form>  
+</body>
+ 
+</html>
---
+++ upload.php
@@ -0,0 +1,15 @@
+<!-- 
+	Made with love by Kunaal Jain
+	Email: kunaalus@gmail.com 
+-->
+<?php
+
+$ds = DIRECTORY_SEPARATOR;
+$storeFolder = 'uploads'; 
+if (!empty($_FILES)) {
+    $tempFile = $_FILES['file']['tmp_name'];
+    $targetPath = dirname( __FILE__ ) . $ds. $storeFolder . $ds;
+    $targetFile =  $targetPath. $_FILES['file']['name'];
+    move_uploaded_file($tempFile,$targetFile);
+}
+?> 
